a7f91f41c210f612009df1e08cd1f516a0d46e19
hi-asamin <asamin.itconsultant@gmail.com>
2023-10-29 16:06:35 +0900

    TikTokとインスタのURL追加

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,7 @@
 export const {
   SITE_NAME = 'Luana Stay',
   SITE_DESCRIPTION = 'Sample',
-  BLOG_URL = 'https://luanastay.com/',
+  BLOG_URL = 'https://luanastay.com',
+  TIKTOK_URL = 'https://www.tiktok.com/@luanastay',
+  INSTAGRAM_URL = 'https://www.instagram.com/luana_hawaii',
 } = process.env;
