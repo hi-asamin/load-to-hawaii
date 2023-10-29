acb91beb477313fe8b1862c00c5929720650cc37
hi-asamin <asamin.itconsultant@gmail.com>
2023-10-29 18:22:45 +0900

    LINEメッセージ送信機能のベース作成

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,7 +1,32 @@
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
-  const body: ContactApiRequestBody = await req.json();
-  console.log(body.email);
-  return NextResponse.json(body, { status: 200 });
+  const { LINE_USER_ID, LINE_CHANNEL_ACCESS_TOKEN } = process.env;
+  const requestBody: ContactApiRequestBody = await req.json();
+  console.log(requestBody.email);
+  fetch('https://api.line.me/v2/bot/message/push', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${LINE_CHANNEL_ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify({
+      to: LINE_USER_ID,
+      messages: [{ type: 'text', text: requestBody }],
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.error('サーバーエラー');
+        return NextResponse.json({ message: 'Failed to send Line' }, { status: 400 });
+      }
+      // ここに成功時の処理を記述
+      console.log('Success:', response.json());
+      return NextResponse.json({ status: 201 });
+    })
+    .catch((error) => {
+      console.log('!!!!failed!!!!');
+      console.error('Error:', error);
+      return NextResponse.json({ message: 'Failed to send Line' }, { status: 500 });
+    });
 }
